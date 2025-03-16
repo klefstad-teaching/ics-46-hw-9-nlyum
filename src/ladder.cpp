f59f6b9b3340d0d3d2f2c20b1426ffa83aed2dcf
nlyum <nlyum@uci.edu>
2025-03-16 08:52:57 +0000

    word ladder prints with end line

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -134,8 +134,14 @@ void load_words(set<string> & word_list, const string& file_name) {
 }
 
 void print_word_ladder(const vector<string>& ladder) {
+    if (ladder.empty()) {
+        cout << "No word ladder found." << endl;
+        return;
+    }
+    
     for (string word: ladder)
         cout << word << ", ";
+    cout << endl;
 }
 
 
