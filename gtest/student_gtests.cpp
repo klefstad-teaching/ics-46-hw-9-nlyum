96ef343c783bcf3e4a2cef40b763ddf9c06edf04
nlyum <nlyum@uci.edu>
2025-03-16 00:54:48 +0000

    added ladder gtests

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -6,3 +6,19 @@
 TEST(Example, ShouldPass) {
   EXPECT_TRUE(true);
 }
+
+TEST(WordLadder, CatToDog) {
+  set<string> word_list = {"cat", "dog", "cot", "cog", "dog"};
+  EXPECT_TRUE(generate_word_ladder("cat", "dog", word_list).size() == 4);
+}
+
+TEST(WordLadder, ExtendString) {
+  set<string> word_list = {"cat", "cats", "catss", "catsss", "catssss"};
+  EXPECT_TRUE(generate_word_ladder("cat", "catssss", word_list).size() == 5);
+}
+
+
+TEST(WordLadder, ShrinkString) {
+  set<string> word_list = {"cat", "c", "ca"};
+  EXPECT_TRUE(generate_word_ladder("cat", "c", word_list).size() == 3);
+}
