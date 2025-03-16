2a2ade353920f763a8c27bc6d57eac3fd10f7dc3
nlyum <nlyum@uci.edu>
2025-03-16 00:04:18 +0000

    moved map creation to separate function

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -11,6 +11,34 @@ void error(string word1, string word2, string msg) {
     cout << word1 << " " << word2 << " ERROR: " << msg << endl;
 }
 
+vector<string> get_patterns(const string& word) {
+    vector<string> patterns;
+    int word_length = word.length();
+    for (int i = 0; i < word_length; ++i) {     // iterate through characters
+        string pattern;
+        pattern += word.substr(0, i);
+        pattern += "*";
+        pattern += word.substr(i + 1);
+        patterns.push_back(pattern);      // push pattern for each character change
+    }
+
+    for (int i = 0; i <= word_length; ++i) {
+        string pattern;
+        pattern += word.substr(0, i);
+        pattern += "*";
+        pattern += word.substr(i);
+        patterns.push_back(pattern);      // push pattern for each character addition
+    }
+
+    for (int i = 0; i < word_length; ++i) {
+        string pattern;
+        pattern += word.substr(0, i);
+        pattern += word.substr(i + 1);
+        patterns.push_back(pattern);      // push pattern for each character removal
+    }
+}
+
+
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d)  {
 
 }
@@ -24,32 +52,11 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
     map<string, vector<string>> patterns;
     map<string, vector<string>> neighbors;
     
-    for (string word : word_list) {
-        int word_length = word.length();
-        for (int i = 0; i < word_length; ++i) {     // iterate through characters
-            string pattern;
-            pattern += word.substr(0, i);
-            pattern += "*";
-            pattern += word.substr(i + 1);
-            patterns[word].push_back(pattern);
-            neighbors[pattern].push_back(word);      // push pattern for each character change
-        }
-
-        for (int i = 0; i <= word_length; ++i) {
-            string pattern;
-            pattern += word.substr(0, i);
-            pattern += "*";
-            pattern += word.substr(i);
-            patterns[word].push_back(pattern);
-            neighbors[pattern].push_back(word);      // push pattern for each character addition
-        }
-
-        for (int i = 0; i < word_length; ++i) {
-            string pattern;
-            pattern += word.substr(0, i);
-            pattern += word.substr(i + 1);
-            patterns[word].push_back(pattern);
-            neighbors[pattern].push_back(word);      // push pattern for each character removal
+    for (string word : word_list) {     // create maps
+        vector<string> word_patterns = get_patterns(word);
+        patterns[word] = word_patterns;
+        for (string pattern : word_patterns) {
+            neighbors[pattern] = word;
         }
     }
     
